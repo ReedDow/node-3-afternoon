f42b470b6530620ff9f809d77af579ea088afbf8
ReedDow <reed.r.dow@gmail.com>
2020-07-02 11:49:04 -0600

    finished

---
+++ products_controller.js
@@ -0,0 +1,55 @@
+module.exports = {
+    create: (req, res, next) => {
+        const dbInstance = req.app.get('db');
+        const{name, description, price, image_url} = req.body;
+        dbInstance.create_product([name, description, price, image_url])
+            .then( () => res.sendStatus(200) )
+            .catch( err => {
+                res.status(500).send({errorMessage: "Malfunction!"});
+                console.log(err)
+        } );
+    },
+
+    getOne: (req, res, next) => {
+        const dbInstance = req.app.get('db')
+        const{id} = req.params;
+        dbInstance.read_product(id)
+            .then( product => res.status(200).send( product ) )
+            .catch( err => {
+                res.status(500).send({errorMessage: "Error!"});
+                console.log(err)
+      } );
+    },
+
+    getAll: (req, res, next) => {
+        const dbInstance = req.app.get('db')
+        dbInstance.read_products()
+            .then( products => res.status(200).send( products ) )
+            .catch( err => {
+                res.status(500).send({errorMessage: "Code Red!"});
+                console.log(err)
+            } );
+    },
+
+    update: (req, res, next) => {
+        const dbInstance = req.app.get('db')
+        const {params, query} = req;
+        dbInstance.update_product([params.id, query.desc])
+            .then( () => res.sendStatus(200) )
+            .catch( err => {
+                res.status(500).send({errorMessage: "Error, Self Destruct Initiated!"});
+                console.log(err)
+            } );
+    },
+
+    delete: (req, res, next) => {
+        const dbInstance = req.app.get('db')
+        const {id} = req.params;
+        dbInstance.delete_product(id)
+            .then( () => res.sendStatus(200) )
+            .catch( err => {
+                res.status(500).send({errorMessage: "Error, Missile Launch Sequence Initiated"});
+                console.log(err)
+            } );
+    }
+};
